34292d6f23dd30451fdc8c323988d59e2c15fd7d
Matthew Fenelon <matt@fnln.me>
2014-02-26 17:35:04 +0000

    One product from the entire store can now be selected on the order screen.

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -3,6 +3,37 @@ Spree::OrdersController.class_eval do
     helper 'spree/taxons'
 
     alias_method :edit_original, :edit
+    alias_method :update_original, :update
+
+    def update
+        if params[:product]
+            @order = current_order(create_order_if_necessary: true)
+            variant_id = params[:product].to_i
+
+            line_item = @order.line_items.detect { |line_item| line_item.variant_id == variant_id }
+
+            if line_item.blank?
+                logger.debug "product not found in order, populating the order with the product - #{variant_id}"
+                quantity = 1
+                populate_params = {:variants => {variant_id => quantity}}
+
+                populator = Spree::OrderPopulator.new(@order, current_currency)
+                if populator.populate(populate_params)
+                    @order.ensure_updated_shipments
+
+                    fire_event('spree.cart.add')
+                end
+
+                line_item = @order.line_items.detect { |line_item| line_item.variant_id == variant_id } 
+            end
+
+            params[:order] = {:line_items_attributes =>
+                @order.line_items.map { |li| {:id => li.id, :quantity => li.variant_id == variant_id ? "1" : "0"} }
+            }
+        end
+
+        update_original
+    end
 
     def edit
         edit_original
