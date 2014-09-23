e8ebd1531f7aa0bbbff7e4db4fcee38c3f5eff25
Matthew Fenelon <matt@fnln.me>
2014-09-23 10:28:03 +0100

    Replace 4 spaces with 2.

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -39,54 +39,54 @@ Spree::OrdersController.class_eval do
       end
 
       def process_selected_product(params)
-          if params[:product] == nil
-              return true
-          end
+        if params[:product] == nil
+            return true
+        end
 
-          variant_id = params[:product].to_i
+        variant_id = params[:product].to_i
 
-          @order = current_order(create_order_if_necessary: true)
+        @order = current_order(create_order_if_necessary: true)
 
-          if add_variant_to_order_if_not_in_order(variant_id, @order) == false
-              return
-          end
+        if add_variant_to_order_if_not_in_order(variant_id, @order) == false
+            return
+        end
 
-          ensure_order_only_contains_variant(params, @order, variant_id)
+        ensure_order_only_contains_variant(params, @order, variant_id)
 
-          return true
+        return true
       end
 
       def add_variant_to_order_if_not_in_order(variant_id, order)
-          if order.find_line_item_by_variant_id(variant_id).blank?
-              return add_variant_to_order(variant_id, order)
-          end
+        if order.find_line_item_by_variant_id(variant_id).blank?
+            return add_variant_to_order(variant_id, order)
+        end
       end
 
       def add_variant_to_order(variant_id, order)
-          logger.debug "Adding product variant:#{variant_id} to the order"
-          quantity = 1
-          populate_params = {:variants => {variant_id => quantity}}
+        logger.debug "Adding product variant:#{variant_id} to the order"
+        quantity = 1
+        populate_params = {:variants => {variant_id => quantity}}
 
-          populator = Spree::OrderPopulator.new(order, current_currency)
-          if populator.populate(populate_params)
-              order.ensure_updated_shipments
+        populator = Spree::OrderPopulator.new(order, current_currency)
+        if populator.populate(populate_params)
+            order.ensure_updated_shipments
 
-              fire_event('spree.cart.add')
-          else
-              logger.debug "Failed to add product variant:#{variant_id} to the order"
-              flash[:error] = populator.errors.full_messages.join(" ")
-              redirect_to cart_path and return false
-          end
+            fire_event('spree.cart.add')
+        else
+            logger.debug "Failed to add product variant:#{variant_id} to the order"
+            flash[:error] = populator.errors.full_messages.join(" ")
+            redirect_to cart_path and return false
+        end
 
-          return true
+        return true
       end
 
       def ensure_order_only_contains_variant(params, order, variant_id)
-          # Change the parameters so that all line items have a quantity of 0, except for the variant.
-          params[:order] = {:line_items_attributes =>
-              order.line_items.map { |li| {:id => li.id, :quantity => li.variant_id == variant_id ? "1" : "0"} },
-              :coupon_code => params[:order][:coupon_code]
-          }
+        # Change the parameters so that all line items have a quantity of 0, except for the variant.
+        params[:order] = {:line_items_attributes =>
+            order.line_items.map { |li| {:id => li.id, :quantity => li.variant_id == variant_id ? "1" : "0"} },
+            :coupon_code => params[:order][:coupon_code]
+        }
       end
 
       def selected_variant_or_default(order)
@@ -94,17 +94,17 @@ Spree::OrdersController.class_eval do
       end
 
       def calculate_order_total_for_variant(order, variant, currency)
-          variant_cost = variant.price_in(currency).amount
-          order_total = Spree::Money.new((order.total - order.item_total) + variant_cost, { currency: currency })
-          return order_total
+        variant_cost = variant.price_in(currency).amount
+        order_total = Spree::Money.new((order.total - order.item_total) + variant_cost, { currency: currency })
+        return order_total
       end
 
       def calculate_per_item_price(product, currency)
-          items_per_product = product.property('totalNumber').to_i
-          if !items_per_product || items_per_product == 0
-              items_per_product = 1
-          end
-          per_item_cost = product.master.price_in(currency).amount / items_per_product
-          return Spree::Money.new(per_item_cost, { currency: currency, no_cents: false })
+        items_per_product = product.property('totalNumber').to_i
+        if !items_per_product || items_per_product == 0
+            items_per_product = 1
+        end
+        per_item_cost = product.master.price_in(currency).amount / items_per_product
+        return Spree::Money.new(per_item_cost, { currency: currency, no_cents: false })
       end
 end
